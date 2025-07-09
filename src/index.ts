6eaeec2773fd3802f0704ecb799cf0ab94687a24
Duc Huy <huynguyen07080401@gmail.com>
2025-07-09 16:38:43 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,7 @@ databaseService
     process.exit(1)
   })
 const app = express()
+app.use(cors())
 
 // initFolder()
 
@@ -85,7 +86,6 @@ app.use(express.json({ limit: '4mb' }))
 //   })
 // )
 app.use(helmet())
-app.use(cors())
 app.use(
   logger('dev', {
     skip: (req: Request) => req.url.indexOf('socket') >= 0
